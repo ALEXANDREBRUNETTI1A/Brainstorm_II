d2d2d39abc06b0769df5dfeb523aa05305527af4
ALEXANDREBRUNETTI1A <165664490+ALEXANDREBRUNETTI1A@users.noreply.github.com>
2026-09-21 12:23:51 -0300

    s

--- main.js
+++ main.js
@@ -121,4 +121,12 @@ function mostraResultado(){
     caixaAlternativas.textContent = ""; 
 }
 
+
+function aleatorio(lista) {
+const posicao = Math.floor(Math.random()* lista.length);
+return lista[posicao];
+
+}
+
+
 mostraPergunta();
